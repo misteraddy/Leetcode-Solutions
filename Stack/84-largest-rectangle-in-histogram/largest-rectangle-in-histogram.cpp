6a5a11fdc5bb63dda39f3c3cec49a6875da4e62d
Aditya singh <121515934+audible123@users.noreply.github.com>
2023-11-19 01:43:47 +0530

    Time: 184 ms (9.78%) | Memory: 85.1 MB (32.49%) - LeetSync

---
+++ Stack/84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -0,0 +1,81 @@
+class Solution {
+public:
+    // Function to find the previous smaller elements for each element in the input vector.
+    // Time complexity: O(N), where N is the size of the input vector.
+    // Space complexity: O(N), for the stack and output vector.
+    vector<int> prevSmallerElements(vector<int>& heights) {
+        int size = heights.size();
+        vector<int> ans(size);
+        stack<int> st;
+        st.push(-1);
+
+        for(int i = 0; i < size; i++) {
+            int curr = heights[i];
+
+            while (st.top() != -1 && heights[st.top()] >= curr) {
+                st.pop();
+            }
+
+            ans[i] = st.top();
+
+            st.push(i);
+        }
+
+        return ans;
+    }
+
+    // Function to find the next smaller elements for each element in the input vector.
+    // Time complexity: O(N), where N is the size of the input vector.
+    // Space complexity: O(N), for the stack and output vector.
+    vector<int> nextSmallerElements(vector<int>& heights) {
+        int size = heights.size();
+        vector<int> ans(size);
+        stack<int> st;
+        st.push(-1);
+
+        for(int i = size - 1; i >= 0; i--) {
+            int curr = heights[i];
+
+            while (st.top() != -1 && heights[st.top()] >= curr) {
+                st.pop();
+            }
+
+            ans[i] = st.top();
+
+            st.push(i);
+        }
+
+        return ans;
+    }
+
+    // Function to find the largest rectangle area formed by the input vector of heights.
+    // Time complexity: O(N), where N is the size of the input vector.
+    // Space complexity: O(N), for the output vector and auxiliary vectors used in nextSmallerElements and prevSmallerElements.
+    int largestRectangleArea(vector<int>& heights) {
+        vector<int> areaArr(heights.size());
+
+        // Find the next smaller elements for each element in the input vector.
+        vector<int> next = nextSmallerElements(heights);
+        for(int i = 0; i < heights.size(); i++) {
+            if(next[i] == -1) {
+                next[i] = heights.size();
+            }
+        }
+
+        // Find the previous smaller elements for each element in the input vector.
+        vector<int> prev = prevSmallerElements(heights);
+
+        // Calculate the area for each element and store it in the areaArr vector.
+        for(int i = 0; i < heights.size(); i++) {
+            int length = heights[i];
+            int width = next[i] - prev[i] - 1 ;
+            int area = length * width;
+            areaArr[i] = area;
+        }
+
+        // Find the maximum area in the areaArr vector.
+        int maxElement = *max_element(areaArr.begin(), areaArr.end());
+
+        return maxElement;
+    }
+};
