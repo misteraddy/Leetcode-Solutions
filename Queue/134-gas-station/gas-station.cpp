947a5006bf60cfe05400dbbdef2923aa5e4b9f96
Aditya singh <121515934+audible123@users.noreply.github.com>
2023-12-01 07:17:35 +0530

    Time: 99 ms (92.66%) | Memory: 128.5 MB (67.86%) - LeetSync

--- Queue/134-gas-station/gas-station.cpp
+++ Queue/134-gas-station/gas-station.cpp
@@ -1,11 +1,25 @@
 class Solution {
     
+    /*
+Intuition
+We have to find a particular position such that if we started from that position, we can cover all the gas stations with enough amount of fuel.
+Suppose there are two gas stations A and B, if A can reach B, then all the stations in between can also reach B. Also if we consider that A cannot reach B, and B is the first station where A cannot reach, then there are chances that any stations in between A and B can also never reach B as there will not be enough gas available. So we need to consider starting from the gas station after B.
+Also if the gas we used to cover all the stations is greater than the cost of gas we paid, then we are surely having a station that will satisfy our condition.
+
+*/
+    
+    
     //ISS QUESTION KI DIKKAT YE HAI KI TUMHE INTUITION NHI DEVELOP HOGI KI
     //ISME QUEUE USE HOGA
     
+    //ERROR JOH MAI KAR RAHA THA
+    //deficit mei add hi nhi kar rha tha mai 
+    //then balance - deficit check karna tha ki 0 se bada hai yaa nhi
+    //agar hai toh start return karo nhi toh -1
+    
     //TIME CCMPLEXITY - O(N)
     
-    //SPACE COMPLEXITY - O()
+    //SPACE COMPLEXITY - O(1)
     
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
