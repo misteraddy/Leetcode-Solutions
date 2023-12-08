09a0f6dc974e6a99753b25156eebdcd3baefff82
Aditya singh <121515934+audible123@users.noreply.github.com>
2023-12-08 15:59:50 +0530

    Time: 5 ms (70.39%) | Memory: 11.6 MB (86.78%) - LeetSync

--- LinkedList/138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ LinkedList/138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -18,6 +18,93 @@ class Solution {
 public:
     Node* copyRandomList(Node* head) {
         
+        if (head == NULL) {
+            return NULL;
+        }
+        
+        Node* temp = head;
+        
+        // Duplicate each node and insert it between the original nodes
+        while (temp != NULL) {
+            Node* clonedNode = new Node(temp->val);
+            clonedNode->next = temp->next;
+            temp->next = clonedNode;
+            temp = temp->next->next;
+        }
+        
+        // Assign random links for the cloned nodes
+        temp = head;
+        while (temp != NULL) {
+            if (temp->random != NULL) {
+                temp->next->random = temp->random->next;
+            }
+            temp = temp->next->next;
+        }
+        
+        // Separate the original and cloned nodes
+        Node* newHead = head->next;
+        temp = head;
+        
+        while (temp != NULL) {
+            Node* clonedNode = temp->next;
+            temp->next = clonedNode->next;
+            
+            if (clonedNode->next != NULL) {
+                clonedNode->next = clonedNode->next->next;
+            }
+            
+            temp = temp->next;
+        }
+        
+        return newHead;
+    }
+};
+
+
+//APPROACH
+
+/*
+
+    Clone and Interleave:
+
+    Traverse the original linked list, and for each node encountered:
+    Create a new node (clonedNode) with the same value as the original node.
+    Insert the clonedNode between the original node and its next node.
+    This effectively creates a new linked list where each node in the original list     is followed by its clone.
+    
+    Assign Random Links for Cloned Nodes:
+
+    Separate Original and Cloned Lists:
+    
+    Return the Head of Cloned List:
+
+    Memory Cleanup:
+
+*/
+
+//ERROR THAT I HAD MADE 
+
+/*
+ 
+ head = head -> next kar diya tha jiski vajah se 
+ ye problem ho rhi thi ki deep copy waala hi remove ho jaa raha tha 
+ 
+ Memory Leak: The code doesn't free the memory of the cloned nodes correctly. After removing the previous nodes in the last loop, the original head node still points to the first cloned node, but the memory of the first cloned node is not freed. This could result in a memory leak.
+
+*/
+
+
+
+//TIME COMPLEXITY - O(N)
+
+//SPACE COMPLEXITY - O(1)
+
+
+/*
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        
         if (!head) {
             return nullptr; // Handle the case when the original list is empty
         }
@@ -56,11 +143,11 @@ public:
 
 
 
+*/
 
+//TIME COMPLEXITY - O(N)
 
-
-
-
+//SPACE COMPLEXITY - O(N) KYUNKI MAP use kar rahe issiliye
 
 /*
 
