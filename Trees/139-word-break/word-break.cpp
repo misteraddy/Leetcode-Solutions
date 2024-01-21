7b4e3bd9000159f5e5b328cabccd96e3137851d8
Aditya singh <121515934+audible123@users.noreply.github.com>
2024-01-21 14:45:23 +0530

    Time: 39 ms (9.77%) | Memory: 18.3 MB (11.88%) - LeetSync

--- Trees/139-word-break/word-break.cpp
+++ Trees/139-word-break/word-break.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
 
-        int n = s.size();
+        // int n = s.size();
         
-        vector<int> dp(n,(-1));
+        // vector<int> dp(n,(-1));
 
-        bool ans = solveUsingMemo(s,wordDict,0,dp);
+        bool ans = solveUsingMemo(s,wordDict);
 
         return ans ;
     }
 
+#if 0
     bool solveUsingMemo(string s,vector<string>& wordDict,int start,vector<int>& dp)
     {
         if(start == s.size())
@@ -42,6 +43,36 @@ public:
         return flag;
     }
 
+#endif
+
+    bool solveUsingMemo(string s,vector<string>& wordDict)
+    {
+        vector<int> dp(s.size() + 1);
+
+        int index = s.size();
+
+        dp[index] = true ;
+
+        for(int start = s.size() - 1 ; start >= 0 ; start--)
+        {
+            bool flag = false;
+            string word = "";
+            for(int i = start ; i < s.size() ; i++)
+            {
+                word += s[i];
+
+                if(check(wordDict,word))
+                {
+                    flag = flag || dp[i + 1];
+                }
+            }
+
+            dp[start] = flag ;
+        }
+
+        return dp[0];
+    }
+
     bool check(vector<string>& wordDict,string word)
     {
         for(auto i : wordDict)
@@ -54,4 +85,6 @@ public:
 
         return false;
     }
+
+
 };
