d77a5e5c0936748a82d3b9b52f20eaee31d85a9f
Aditya singh <121515934+audible123@users.noreply.github.com>
2024-01-22 19:01:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (40.60%) - LeetSync

---
+++ Trees/909-stone-game/stone-game.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+
+        //100 mei se agar ek tareeka bhi hoga jisme alice jeetagi toh usme true return hogaa koi naa koi pattern har pile mei hogaa 
+    }
+};
