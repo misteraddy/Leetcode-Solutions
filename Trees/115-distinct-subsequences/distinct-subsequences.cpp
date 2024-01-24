f3b292bb49d956ceb9ba1d5777f99e8c9dd7fd93
Aditya singh <121515934+audible123@users.noreply.github.com>
2024-01-24 17:01:30 +0530

    Time: 21 ms (85.41%) | Memory: 19.8 MB (44.84%) - LeetSync

--- Trees/115-distinct-subsequences/distinct-subsequences.cpp
+++ Trees/115-distinct-subsequences/distinct-subsequences.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int numDistinct(string s, string t) {
 
-        vector<vector<int>> dp(s.length() + 1,vector<int>(t.length() + 1,(-1)));
+        // vector<vector<int>> dp(s.length() + 1,vector<int>(t.length() + 1,(-1)));
         
-        int ans = solve(s,t,0,0,dp);
+        int ans = solve(s,t);
 
         return ans;
     }
 
+#if 0
     int solve(string s,string t,int i,int j,vector<vector<int>>& dp)
     {
         if(j == t.size())
@@ -37,4 +38,41 @@ public:
 
         return dp[i][j] = ans;
     }
+
+#endif
+
+
+   int solve(string s, string t)
+    {
+        int m = t.size();
+        int n = s.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));   // -1 se initialised
+
+        for (int i = 0; i <= n; i++)  //i < n
+        {
+            dp[i][m] = 1; //dp[i][m - 1] = 1; error
+        }
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = m - 1; j >= 0; j--)
+            {
+                long long ans = 0;   //int 32 bit k paar pahuch rhai thi issiliye long long liya
+
+                if (s[i] == t[j])
+                {
+                    ans += dp[i + 1][j + 1];
+                }
+
+                ans += dp[i + 1][j];
+
+                dp[i][j] = ans;
+            }
+        }
+
+        return dp[0][0];
+    }
+
+
 };
