e0ecd7d7c76f4e8c7a3c710e2d314a3b00849d7e
Aditya singh <121515934+audible123@users.noreply.github.com>
2024-01-25 17:42:14 +0530

    Time: 27 ms (84.86%) | Memory: 25.6 MB (9.01%) - LeetSync

--- Trees/1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ Trees/1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
 
-        int n = text1.size();
+        // int n = text1.size();
 
-        int m = text2.size();
+        // int m = text2.size();
 
-        vector<vector<int>>dp(n,vector<int>(m,-1)); //INT_MAX se bhi initialise kar sakte hai 
+        // vector<vector<int>>dp(n,vector<int>(m,-1)); //INT_MAX se bhi initialise kar sakte hai 
         
-        int ans = solve(text1,text2,0,0,dp);
+        int ans = solve(text1,text2);
 
         return ans;
     }
 
+#if 0
     int solve(string& text1,string& text2,int i,int j,vector<vector<int>>& dp)
-    {
+    {   
         if(i >= text1.length())
         {
             return 0 ;
@@ -45,4 +46,39 @@ public:
 
         return dp[i][j];
     }
+#endif
+
+    int solve(string& text1,string& text2)
+    {   
+
+        int n = text1.length() ;
+
+        int m = text2.length() ;
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+//sabko 0 se inittialise
+
+        //otherwise hum log loop lagake last col and last row ki saari values ko 0 kar dete hai
+
+        int ans = 0;
+        for(int i = n - 1 ; i >= 0 ; i-- )
+        {
+            for(int j = m - 1 ; j >= 0 ; j-- )
+            {
+                if(text1[i] == text2[j])
+                {
+                     ans = 1 + dp[i+1][j+1];
+                }
+                else
+                {
+                    ans = 0 + max(dp[i+1][j],dp[i][j+1]);
+                }
+
+                dp[i][j] = ans ;
+            }
+        }
+
+        return dp[0][0];
+    }
+
 };
