a23269e12b9eabc202ef478350414420385b2758
Aditya singh <121515934+audible123@users.noreply.github.com>
2024-02-04 11:40:47 +0530

    Time: 11 ms (74.57%) | Memory: 10.7 MB (28.40%) - LeetSync

--- DP/1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ DP/1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
+
+#if 0
+
     vector<int>dp;
+
     int f(int ind, vector<int>&v, int k){
         int n = v.size();
         if(ind>=n){
@@ -18,9 +22,49 @@ public:
         }
         return dp[ind] = s;
     }
+
+#endif
+
+    //tabulation
+
+    int f(vector<int>&v, int k)
+    {
+        int n = v.size() ;
+
+        vector<int> dp(n + 1,-1);
+
+        dp[n] = 0 ;
+
+        for(int ind = n - 1 ; ind >= 0 ; ind--)
+        {
+            int maxi = 0 ;
+
+            int s = 0 ;
+
+            for(int i = ind ; i < n ; i++)
+            {
+                maxi = max(maxi,v[i]);
+
+                if(i - ind + 1 <= k)
+                {
+                    s = max(s, maxi*(i - ind + 1) + dp[ i + 1]);
+                }
+            }
+
+            dp[ind] = s ;
+        }
+
+        return dp[0];
+    }
+
+    
     int maxSumAfterPartitioning(vector<int>& v, int k) {
         int n = v.size();
-        dp.assign(n,-1);
-        return f(0,v,k);
+        // dp.assign(n,-1);
+        // return f(0,v,k);
+
+        int ans = f(v,k);
+
+        return ans ;
     }
 };
