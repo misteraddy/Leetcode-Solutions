87024e18804f17b01ed96b3dc40c929420c96b15
Aditya singh <121515934+audible123@users.noreply.github.com>
2024-02-11 10:41:50 +0530

    Time: 315 ms (32.71%) | Memory: 185.1 MB (5.21%) - LeetSync

---
+++ DP/2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNodes(ListNode* head) {
+        ListNode* temp = head;
+        stack<int> st;
+        vector<int> arr;
+
+        while(temp != NULL) {
+            int element = temp->val;
+            arr.push_back(element);
+            temp = temp->next;
+        }
+
+        for(int i = 0 ; i < arr.size() ; i++) {
+            while(!st.empty() && st.top() < arr[i]) {
+                st.pop();
+            }
+            st.push(arr[i]);
+        }
+
+        ListNode dummyNode;
+        dummyNode.val = (-1);
+
+        while(!st.empty()) {
+            int val = st.top(); 
+            st.pop();
+            ListNode* newNode = new ListNode(val); // Allocate memory dynamically
+            newNode->next = dummyNode.next;
+            dummyNode.next = newNode;
+        }
+
+        return dummyNode.next;
+    }
+};
+
+//Error
+/*
+
+Line 38: Char 19: error: cannot initialize a variable of type 'ListNode *' with an rvalue of type 'int'
+   38 |         ListNode* dummyNode(-1);
+      |                   ^         ~~
+
+
+
+*/
