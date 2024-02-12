d464c184934689f8c83340d5452756e5e9b6bfeb
Aditya singh <121515934+audible123@users.noreply.github.com>
2024-02-12 12:43:16 +0530

    Time: 156 ms (77.20%) | Memory: 66.8 MB (69.63%) - LeetSync

---
+++ DP/1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+
+    class Coordinate{
+        public:
+            int x ;
+            int y ;
+            int dist ;
+
+        Coordinate(int dist,int x,int y)
+        {
+            this -> x = x;
+            this -> y = y;
+            this -> dist = dist;
+        }
+        
+    };
+
+    class Compare{
+
+        public:
+
+        bool operator()(const Coordinate& a,const Coordinate& b)
+        {
+            return a.dist < b.dist;
+        }
+    };
+
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+
+        priority_queue<Coordinate, vector<Coordinate>, Compare > pq;
+        
+        for(int i = 0 ; i < points.size() ; i++)
+        {
+            int x = points[i][0];
+            int y = points[i][1];
+
+            int dist = (x*x + y*y) ;
+
+            Coordinate point(dist,x,y);
+
+            pq.push(point);
+
+            if(pq.size() > k)
+            {
+                pq.pop();
+            }
+        }
+
+        vector<vector<int>> ans ;
+
+        while(!pq.empty())
+        {
+            int x = pq.top().x;
+            int y = pq.top().y;
+
+            ans.push_back({x,y});
+
+            pq.pop();
+        }
+
+        return ans ;
+
+    }
+}; 
