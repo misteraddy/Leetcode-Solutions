89794c52d510a3be6f573064ea93868969470591
Aditya singh <121515934+audible123@users.noreply.github.com>
2024-02-13 06:07:35 +0530

    Time: 40 ms (91.70%) | Memory: 26.6 MB (24.97%) - LeetSync

--- DP/2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
+++ DP/2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
+    // Function to check if a given string is a palindrome.
     bool check(string s)
     {
         int start=0;
         int end=s.length()-1;
+        // Loop until the start index is less than the end index.
         while(start<end)
         {
+            // If characters at start and end indices are not equal, return false.
             if(s[start]!=s[end])
                 return false;
+            // Move start index forward and end index backward.
             start++;
             end--;
         }
+        // If loop completes without returning false, the string is a palindrome, so return true.
         return true;
     }
+    
+    // Function to find the first palindrome in a vector of strings.
     string firstPalindrome(vector<string>& words) 
     {
+        // Iterate through the vector of strings.
         for(int i=0;i<words.size();i++)
         {
+            // If the current string is a palindrome, return it.
             if(check(words[i]))
                 return words[i];
         }
+        // If no palindrome is found, return an empty string.
         return "";
     }
-};
+}; 
