b37d52bdaf4668e7bcc90b667faf40a2e0325373
Aditya singh <121515934+audible123@users.noreply.github.com>
2024-02-23 23:43:03 +0530

    Time: 16 ms (69.71%) | Memory: 16.6 MB (51.70%) - LeetSync

---
+++ DP/803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        
+        vector<int> distance(n,INT_MAX);
+
+        unordered_map<int,vector<pair<int,int>>> adj ;
+
+        for(auto &vec : flights)
+        {
+            int u = vec[0];
+
+            int v = vec[1];
+
+            int cost = vec[2];
+
+            adj[u].push_back({v,cost});
+        }
+
+        int level = 0 ;
+
+        queue<pair<int,int>> que ;
+
+        que.push({src,0});
+
+        distance[src] = 0 ;
+
+        while(!que.empty() && level <= k)
+        {
+
+            int N = que.size();
+
+            while(N--)
+            {
+                int u = que.front().first ;
+                int d = que.front().second ;
+                que.pop();
+
+                for(pair<int,int> &P : adj[u])
+                {
+                    int v = P.first ;
+                    int cost = P.second ;
+
+                    if(distance[v] > d + cost)
+                    {
+                        distance[v] = d + cost ;
+                        que.push({v,d + cost});
+                    }
+                }
+            }
+
+            level++;
+        }
+
+        return distance[dst] == INT_MAX ? -1 : distance[dst];
+    }
+};
+
+//ERROR 
+/*
+
+                int u = que.front().first ;
+                int d = que.front().second ; 
+
+                WERE NOT INSIDE THE NESTED WHILE LOOP
+
+
+*/
