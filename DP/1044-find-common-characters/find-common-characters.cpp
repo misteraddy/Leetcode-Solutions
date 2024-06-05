729ef33bc7ca20d179a9149cc55db06b7a4c9f00
Aditya singh <121515934+audible123@users.noreply.github.com>
2024-06-05 08:06:59 +0530

    Time: 7 ms (62.84%) | Memory: 11.3 MB (73.31%) - LeetSync

--- DP/1044-find-common-characters/find-common-characters.cpp
+++ DP/1044-find-common-characters/find-common-characters.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     vector<string> commonChars(vector<string>& words) {
-         int n= words.size();
-        vector<int>v(26, INT_MAX);
-        vector<string>ans;
+        int n = words.size();
+        vector<int> v(26, INT_MAX); // to store the min frequency of each character
+        vector<string> ans;
 
-        for(int i=0;i<n;i++){
-            vector<int>v1(26, 0);
-            for(int j=0;j<words[i].size();j++){
+        for(int i = 0; i < n; i++) {
+            vector<int> v1(26, 0); // to count frequency of each character in the current word
+            for(int j = 0; j < words[i].size(); j++) {
                 v1[words[i][j] - 'a']++;
             }
-            for(int i=0;i<26;i++){
-                v[i] = min(v[i], v1[i]);
+            for(int k = 0; k < 26; k++) {
+                v[k] = min(v[k], v1[k]); // update the min frequency
             }
         }
 
-        for(int i=0;i<26;i++){
-        for(int j=0;j<v[i];j++){
-            ans.push_back(string(1,i + 'a'));
+        for(int i = 0; i < 26; i++) {
+            while(v[i] > 0) { // while there's at least one occurrence of the character
+                ans.push_back(string(1, 'a' + i)); // convert character to string and add to ans
+                v[i]--;
+            }
         }
-    }
 
-    return ans;
+        return ans;
     }
 };
