0346fdfe326b7637b25676d1be5cf412b8e98a3d
Aditya singh <121515934+misteraddy@users.noreply.github.com>
2024-10-16 00:42:07 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (7.94%) - LeetSync

--- DP/268-missing-number/missing-number.java
+++ DP/268-missing-number/missing-number.java
@@ -1,17 +1,25 @@
 class Solution {
+
     public int missingNumber(int[] nums) {
         
-        int n = nums.length;
+        int xor1 = 0 ;
 
-        int totalSum = (n * (n + 1)) / 2 ;
+        int xor2 = 0 ;
 
-        int sum = 0 ;
+        int n = nums.length ;
 
         for(int i = 0 ; i < n ; i++)
         {
-            sum += nums[i];
+            xor1 = xor1 ^ nums[i];
         }
 
-        return totalSum - sum ;
+        for(int i = 0 ; i <= n ; i++)
+        {
+            xor2 = xor2 ^ i ;
+        }
+
+        return xor2 ^ xor1 ;
     }
+
+    
 }
