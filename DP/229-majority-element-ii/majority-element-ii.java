f8c62f013df91748ca4648b4c1886d6975ef77aa
Aditya singh <121515934+misteraddy@users.noreply.github.com>
2024-10-16 16:27:56 +0530

    Time: 16 ms (11.38%) | Memory: 47.7 MB (65.26%) - LeetSync

--- DP/229-majority-element-ii/majority-element-ii.java
+++ DP/229-majority-element-ii/majority-element-ii.java
@@ -1,74 +1,30 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        
+        Map<Integer,Integer> mp = new HashMap<>();
+
+        List<Integer> ls = new ArrayList<>();
 
         int n = nums.length ;
 
-        List<Integer> ans = new ArrayList<>();
-        
-        int e1 = 0 , e2 = 0 ;
-
-        int cnt1 = 0 , cnt2 = 0 ;
-
-        for(int i = 0 ; i < nums.length ; i++)
-        {
-
-            if(cnt1 == 0 && nums[i] != e2)
-            {
-                e1 = nums[i];
-                cnt1 = 1 ;
-            } else if(cnt2 == 0 && nums[i] != e1)
-            {
-                e2 = nums[i];
-                cnt2 = 1 ;
-            } else if(nums[i] == e1)
-            {
-                cnt1++;
-            } else if(nums[i] == e2)
-            {
-                cnt2++;
-            } else {
-                cnt1--;
-                cnt2--;
-            }
-        }
-
-        cnt1 = 0 ; cnt2 = 0 ;
-
-        for(int i = 0 ; i < nums.length ; i++)
-        {
-            if(nums[i] == e1)
-            {
-                cnt1++;
-            }
-
-            if(nums[i] == e2)
-            {
-                cnt2++;
-            }
-        }
-
         int mini = (int)(n / 3) + 1 ;
 
-        if(e1 == e2)
+        for(int i = 0 ; i < n ; i++)
         {
-            if(cnt1 >= mini)
+            int value = mp.getOrDefault(nums[i],0);
+
+            mp.put(nums[i],value + 1);
+
+            if(mp.get(nums[i]) == mini)
             {
-                ans.add(e1);
+                ls.add(nums[i]);
             }
 
-        } else {
-
-            if(cnt1 >= mini)
-            {
-                ans.add(e1);
+            if(ls.size() == 2) {
+                break;
             }
+        } 
 
-            if(cnt2 >= mini)
-            {
-                ans.add(e2);
-            }
-        }
-
-        return ans ;
+        return ls ;
     }
 }
